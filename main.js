c4c5d68cdfdb22fc3886e7ca8b8588fdc946029a
Gonzalo <gvivanco4@gmail.com>
2025-10-26 18:07:45 -0600

    Defined HTML Structure and CSS Basic Styling. Advance on JS Main Logic

---
+++ main.js
@@ -0,0 +1,15 @@
+// Global variables
+
+const DEFAULT_SIZE = 16;
+const canvas = document.querySelector(".canvas-container")
+
+// Div Creation Function (inside container)
+
+function createCanvaSize (size = DEFAULT_SIZE) {
+    
+    const squareDiv = document.createElement("div")
+
+}
+
+// Establish color tracing mechanic
+
